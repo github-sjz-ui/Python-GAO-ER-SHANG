27c3318e66051a6f5e0e2e8da5894932105be0b9
沈纪中 <69446590+github-sjz-ui@users.noreply.github.com>
2022-12-17 17:45:47 +0800

    Add files via upload

---
+++ 打印菱形.py
@@ -0,0 +1,12 @@
+"""
+https://www.tzcoder.cn/acmhome/problemdetail.do?method=showdetail&id=1172
+"""
+n=int(input('从键盘输入一个整数n(1≤n≤100)，打印出指定的菱形:'))
+for i in range (1,n+1):
+    k=(n-i)*' '
+    t=(2*i-1)*'*'
+    print(k+t)
+for i in range(1,n):
+    k=i*' '
+    t=(-2*i+9)*'*'
+    print(k+t)
