de24324eabd4714038de53362def86bf167a7c96
沈纪中 <69446590+github-sjz-ui@users.noreply.github.com>
2023-04-16 12:26:56 +0800

    Update 素数等差数列.py
    这是高二下编写的

--- 素数等差数列/素数等差数列.py
+++ 素数等差数列/素数等差数列.py
@@ -12,11 +12,20 @@ def L(a):
         if isprime(j):
             list.append(j)
     return list
-list1=L(100)
+def max_len(lis):
+    max_len=0
+    for i in lis:
+        if len(i)>max_len:
+            max_len=len(i)
+    for i in lis:
+        if len(i)==max_len:
+            print(i)
+n=int(input("请输入整数n："))
+list1=L(n)
 data=[]
 r=[]
-for i in range(1,100//2):# 定义公差
-    for j in range(len(list1)):
+for i in range(1,n//2):# 定义公差
+    for j in range(len(list1)):# 根据指针和公差遍历
         while True:
             if data!=[] and data[-1]+i in list1:
                 data.append(data[-1]+i)
@@ -33,4 +42,7 @@ for i in range(1,100//2):# 定义公差
                 break
             
         data=[]
+
 print(r)
+print('最长等差数列为：')
+max_len(r)
